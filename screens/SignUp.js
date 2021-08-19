9226e30e9e32b133754e6cf190316d459a7e9740
Ritwik <ritwik@boringnews.co>
2021-08-19 12:24:03 +0530

    google auth added

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -9,13 +9,49 @@ import {
   View,
   Button,
 } from 'react-native';
-
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+// GoogleSignin.configure({
+//   webClientId:
+//     '1063598088065-cttjjma21ibpfn7mmfhuhv0vot3ds3in.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+//   offlineAccess: false, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+//   forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+// });
+GoogleSignin.configure();
 import {NavigationContainer} from '@react-navigation/native';
 
-export default function App() {
+export default function SignUp() {
+  let signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+    } catch (error) {
+      console.log(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
   return (
     <View>
       <Text>SignUp</Text>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+      />
     </View>
   );
 }
